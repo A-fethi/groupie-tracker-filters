3801e7be1ec025183b499fe303b4aa8374f26abd
Abderrahmane Fethi <fethiabderrahmane1@gmail.com>
2024-10-21 17:02:39 +0100

    adds filter feature to groupie tracker

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	groupie "groupie/ressources"
+)
+
+func main() {
+	http.HandleFunc("/", groupie.HandleHome)
+	http.HandleFunc("/informations/{id}", groupie.HandleInfos)
+	http.HandleFunc("/search", groupie.HandleSearch)
+	http.HandleFunc("/filters", groupie.HandleFilters)
+	fmt.Println("Starting server on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Printf("Server failed to start: %v", err)
+		return
+	}
+}
